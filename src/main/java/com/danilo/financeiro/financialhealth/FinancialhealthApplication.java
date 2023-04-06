726c4fa4db8155c53b88ef6056bc2f07c707b97d
danilovalerio <danilo_vs@hotmail.com>
2023-04-06 10:02:25 -0300

    Initial commit setup application

---
+++ src/main/java/com/danilo/financeiro/financialhealth/FinancialhealthApplication.java
@@ -0,0 +1,13 @@
+package com.danilo.financeiro.financialhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialhealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialhealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danilo/financeiro/financialhealth/FinancialhealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilo.financeiro.financialhealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialhealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
