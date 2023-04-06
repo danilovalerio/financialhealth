ccc626a7940964490a39c8f4c75059d194019a2a
danilovalerio <danilo_vs@hotmail.com>
2023-04-06 18:00:30 -0300

    Mapeamento da entidade para o Spring

--- src/main/java/com/danilo/financeiro/financialhealth/domain/model/Usuario.java
+++ src/main/java/com/danilo/financeiro/financialhealth/domain/model/Usuario.java
@@ -1,19 +1,41 @@
 package com.danilo.financeiro.financialhealth.domain.model;
 
+import jakarta.persistence.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
 
+@Entity
 public class Usuario implements UserDetails {
+
+    private static final String USUARIO = "usuario";
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idUsuario")
     private Long id;
     private String nome;
+
+    @Column(nullable = false, unique = true)
     private String email;
+    @Column(nullable = false)
     private String senha;
+
+    /**
+     * Por ser uma foto em base64 e ter muitos caracters definimos como TEXTO
+     */
+    @Column(columnDefinition = "TEXT")
     private String foto;
+    @Column(nullable = false)
     private String dataCadastro;
     private String dataInativacao;
+
+    /**
+     * Um usuário pode ter muitos títulos
+     * Toda vez que é uma lista o final é Many
+     */
+    @OneToMany(mappedBy = USUARIO)
     private List<Titulo> titulos;
 
     public Long getId() {
