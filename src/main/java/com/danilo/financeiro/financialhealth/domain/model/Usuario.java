c7bd54e3b7ab1d4320054a39770be99f6b22701e
danilovalerio <danilo_vs@hotmail.com>
2023-04-06 17:48:15 -0300

    Implementacao de Poderes para Usuario

--- src/main/java/com/danilo/financeiro/financialhealth/domain/model/Usuario.java
+++ src/main/java/com/danilo/financeiro/financialhealth/domain/model/Usuario.java
@@ -1,8 +1,12 @@
 package com.danilo.financeiro.financialhealth.domain.model;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 import java.util.List;
 
-public class Usuario {
+public class Usuario implements UserDetails {
     private Long id;
     private String nome;
     private String email;
@@ -65,5 +69,40 @@ public class Usuario {
 
     public void setTitulos(List<Titulo> titulos) {
         this.titulos = titulos;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
     }
 }
