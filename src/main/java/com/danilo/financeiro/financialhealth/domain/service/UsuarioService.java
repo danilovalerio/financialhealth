0c44d5d81e2a07cca3a9f73b8f0ef17ba961c631
danilovalerio <danilo_vs@hotmail.com>
2023-04-10 15:23:42 -0300

    Usuario Service

--- src/main/java/com/danilo/financeiro/financialhealth/domain/service/UsuarioService.java
+++ src/main/java/com/danilo/financeiro/financialhealth/domain/service/UsuarioService.java
@@ -1,4 +1,33 @@
 package com.danilo.financeiro.financialhealth.domain.service;
 
-public class UsuarioService {
+import com.danilo.financeiro.financialhealth.dto.usuario.UsuarioRequestDto;
+import com.danilo.financeiro.financialhealth.dto.usuario.UsuarioResponseDto;
+
+import java.util.List;
+
+public class UsuarioService implements ICRUDService<UsuarioRequestDto, UsuarioResponseDto> {
+    @Override
+    public List<UsuarioResponseDto> obterTodos() {
+        return null;
+    }
+
+    @Override
+    public UsuarioResponseDto obterPorId(Long id) {
+        return null;
+    }
+
+    @Override
+    public UsuarioResponseDto cadastrar(UsuarioRequestDto dto) {
+        return null;
+    }
+
+    @Override
+    public UsuarioResponseDto atualizar(Long id, UsuarioRequestDto dto) {
+        return null;
+    }
+
+    @Override
+    public void deletar(Long id) {
+
+    }
 }
