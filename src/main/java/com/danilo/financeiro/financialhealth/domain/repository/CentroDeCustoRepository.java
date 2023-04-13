1413779f06695da6c069ea1719f689468724526e
danilovalerio <danilo_vs@hotmail.com>
2023-04-13 14:46:36 -0300

    CENTRO DE CUSTO - Repository

---
+++ src/main/java/com/danilo/financeiro/financialhealth/domain/repository/CentroDeCustoRepository.java
@@ -0,0 +1,8 @@
+package com.danilo.financeiro.financialhealth.domain.repository;
+
+import com.danilo.financeiro.financialhealth.domain.model.CentroDeCusto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CentroDeCustoRepository extends JpaRepository<CentroDeCusto, Long> {
+
+}
