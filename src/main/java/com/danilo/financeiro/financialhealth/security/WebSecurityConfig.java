525415de4615fb209cc988c8f1fdf1ae57198556
danilovalerio <danilo_vs@hotmail.com>
2023-04-13 11:24:40 -0300

    WebSecurityConfig - Adicionando os filter de Authorization e Authentication

---
+++ src/main/java/com/danilo/financeiro/financialhealth/security/WebSecurityConfig.java
@@ -0,0 +1,73 @@
+package com.danilo.financeiro.financialhealth.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * @Configuration - Por se tratar de um arquivo configurador
+ * @EnableWebSecurity - componente de segurançca Web
+ *
+ * Não extende mais de nenhuma outra classe nas novas versoes
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
+
+    /**
+     * Criptografador da senha
+     * @return
+     *
+     * @Bean - para usar em qualquer lugar
+     */
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        //Config inicial para autenticação do protocolo HTTP de Autenticacao
+        httpSecurity
+                .headers().frameOptions().disable().and()
+                .cors().and().csrf().disable()
+                .authorizeHttpRequests((auth) ->
+                        //Toda vez que alguém fizer um post para essa URL permitimos todos
+                        auth.requestMatchers(HttpMethod.POST, "/api/usuarios").permitAll()
+                                //para qualquer outro request tem que estar autenticado
+                                .anyRequest()
+                                .authenticated())
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        //Configuracao do filtro para autenticacao e autorizacao
+        httpSecurity.addFilter(new JwtAuthenticationFilter(
+                authenticationManager(authenticationConfiguration), jwtUtil)
+        );
+
+        httpSecurity.addFilter(new JwtAuthorizationFilter(
+                authenticationManager(authenticationConfiguration), jwtUtil)
+        );
+
+        return httpSecurity.build();
+    }
+}
