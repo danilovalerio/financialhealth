c92dd7ddd75bf8d65cc375542b52046f9e53d4f5
danilovalerio <danilo_vs@hotmail.com>
2023-04-13 15:06:45 -0300

    CENTRO DE CUSTO - Service com os Mappers

--- src/main/java/com/danilo/financeiro/financialhealth/domain/service/CentroDeCustoService.java
+++ src/main/java/com/danilo/financeiro/financialhealth/domain/service/CentroDeCustoService.java
@@ -1,33 +1,87 @@
 package com.danilo.financeiro.financialhealth.domain.service;
 
+import com.danilo.financeiro.financialhealth.domain.exception.ResourceNotFoundException;
+import com.danilo.financeiro.financialhealth.domain.model.CentroDeCusto;
+import com.danilo.financeiro.financialhealth.domain.model.Usuario;
+import com.danilo.financeiro.financialhealth.domain.repository.CentroDeCustoRepository;
 import com.danilo.financeiro.financialhealth.dto.centrodecusto.CentroDeCustoRequestDto;
 import com.danilo.financeiro.financialhealth.dto.centrodecusto.CentroDeCustoResponseDto;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class CentroDeCustoService implements ICRUDService<CentroDeCustoRequestDto, CentroDeCustoResponseDto>{
+
+    @Autowired
+    private CentroDeCustoRepository centroDeCustoRepository;
+
+    @Autowired
+    private ModelMapper mapper;
+
     @Override
     public List<CentroDeCustoResponseDto> obterTodos() {
-        return null;
+        List<CentroDeCusto> centrosDeCusto = centroDeCustoRepository.findAll();
+
+        return centrosDeCusto.stream()
+                .map(centroDeCusto -> mapper.map(centroDeCusto, CentroDeCustoResponseDto.class))
+                .collect(Collectors.toList());
     }
 
     @Override
     public CentroDeCustoResponseDto obterPorId(Long id) {
-        return null;
+        Optional<CentroDeCusto> optCentroDeCusto = centroDeCustoRepository.findById(id);
+
+        if (optCentroDeCusto.isEmpty()) {
+            throw new ResourceNotFoundException("Não foi encontrado o centro de custo com esse id "+id);
+        }
+
+        return mapper.map(optCentroDeCusto, CentroDeCustoResponseDto.class);
     }
 
     @Override
     public CentroDeCustoResponseDto cadastrar(CentroDeCustoRequestDto dto) {
-        return null;
+        /**
+         * Pega o Request e convert num centro de custo model
+         */
+        CentroDeCusto centroDeCustoNovo = mapper.map(dto, CentroDeCusto.class);
+
+        /**
+         * Pegar o usuário do context de segurança da autenticação do Spring
+         */
+        Usuario usuario = (Usuario) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        centroDeCustoNovo.setUsuario(usuario);
+        centroDeCustoNovo.setId(null);
+        centroDeCustoNovo = centroDeCustoRepository.save(centroDeCustoNovo);
+
+        return mapper.map(centroDeCustoNovo, CentroDeCustoResponseDto.class);
     }
 
     @Override
     public CentroDeCustoResponseDto atualizar(Long id, CentroDeCustoRequestDto dto) {
-        return null;
+
+        obterPorId(id);
+        CentroDeCusto centroDeCustoNovo = mapper.map(dto, CentroDeCusto.class);
+
+        Usuario usuario = (Usuario) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        centroDeCustoNovo.setUsuario(usuario);
+
+        centroDeCustoNovo.setId(id);
+        centroDeCustoNovo = centroDeCustoRepository.save(centroDeCustoNovo);
+
+        return mapper.map(centroDeCustoNovo, CentroDeCustoResponseDto.class);
     }
 
     @Override
     public void deletar(Long id) {
+        // Já valida se o cara existe
+        obterPorId(id);
+
+        centroDeCustoRepository.deleteById(id);
 
     }
 }
