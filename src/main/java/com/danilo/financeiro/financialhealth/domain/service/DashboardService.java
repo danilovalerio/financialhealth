26db48a677a853c402a8d69931e71a4d1ae28802
danilovalerio <danilo_vs@hotmail.com>
2023-04-14 14:27:12 -0300

    DashboardService - obterFluxoCaixaPorDataVencimento

--- src/main/java/com/danilo/financeiro/financialhealth/domain/service/DashboardService.java
+++ src/main/java/com/danilo/financeiro/financialhealth/domain/service/DashboardService.java
@@ -1,10 +1,12 @@
 package com.danilo.financeiro.financialhealth.domain.service;
 
+import com.danilo.financeiro.financialhealth.domain.Enum.ETipoTitulo;
 import com.danilo.financeiro.financialhealth.dto.dashboard.DashboardResponseDto;
 import com.danilo.financeiro.financialhealth.dto.titulo.TituloResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -19,7 +21,30 @@ public class DashboardService {
 
         List<TituloResponseDto> titulos =
                 tituloService.obterPorDataDeVencimento(periodoInicial, periodoFinal);
+
+        //Tudo que precisamos devolver para o response
+        Double totalApagar = 0.0;
+        Double totalAreceber = 0.0;
+        Double saldo = 0.0;
+
+        List<TituloResponseDto> titulosApagar = new ArrayList<>();
+        List<TituloResponseDto> titulosAreceber = new ArrayList<>();
+
+        for (TituloResponseDto titulo : titulos) {
+            if (titulo.getTipo() == ETipoTitulo.APAGAR){
+                totalApagar += titulo.getValor();
+                titulosApagar.add(titulo);
+            } else  {
+                totalAreceber += titulo.getValor();
+                titulosAreceber.add(titulo);
+            }
+        }
+
+        saldo = totalAreceber - totalApagar;
+
         //Aqui que acontece a Magia kkkkk
-        return new DashboardResponseDto();
+        return new DashboardResponseDto(
+                totalApagar, totalAreceber, saldo, titulosApagar, titulosAreceber
+        );
     }
 }
